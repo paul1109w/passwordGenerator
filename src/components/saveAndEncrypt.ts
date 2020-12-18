030a5fab289811abe273612f5911176e139c9dc6
Paul <weise_paul@t-online.de>
2020-12-18 08:39:03 +0100

    updated the save to use the api

--- src/components/saveAndEncrypt.ts
+++ src/components/saveAndEncrypt.ts
@@ -1,18 +1,44 @@
 import CryptoES from 'crypto-es';
 import {secret} from './consts/constSecret';
+import axios from 'axios';
 
 export class encryptSave {
     private encrypt = (data:string) => {
         const hash = CryptoES.HmacSHA512(data, secret);
         return hash;
     }
+    private checkForValue(json,value:string):boolean {
+        for (let key in json) {
+            if (typeof (json[key]) === "object") {
+                return this.checkForValue(json[key], value);
+            } else if (json[key] === value) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    save = (data:string) => {
-        window.localStorage.setItem(String(this.encrypt(data)), "1");
+    save = async(data:string) => {
+        const hash = this.encrypt(data);
+        const json = {
+            "passwordHASH": String(hash)
+        };
+        let res = await axios.post('http://localhost:3002/passwords', json);
+
+        console.log(res.data);
+
     }
 
     checkIfPasswordAlreadyInside = (data:string) => {
         const hash = String(this.encrypt(data));
-        return window.localStorage.getItem(hash) === null ? false : true;
+        var allExisting = axios.get('http://localhost:3002/passwords').then(response => {
+            return response.data;
+        });
+        var res = allExisting.then(result => {
+            console.log(result)
+        });
+        console.log(res);
+        return this.checkForValue(allExisting,hash);
     }
+
 }
